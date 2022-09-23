e2c0d45ee6717cd72313811ae34bd5b8dc002834
Alka04 <alka04006@utb.vaxjo.se>
2022-09-23 11:00:48 +0200

    Add project files.

---
+++ ovningsuppgfit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ovningsuppgfit
+{ 
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Berätta din ålder för att ta reda på om du får vara med i tävlingen");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får vara med i tävlingen");
+            }
+            
+            else
+            {
+                Console.WriteLine("Du får tyvärr inte vara med i tvävlingen");
+            }
+        }
+    }
+}
